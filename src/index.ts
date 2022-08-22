35e22e1dc5ae39549186b1d792322ea90715f462
nick-cheatwood7 <nick.cheatwood@gmail.com>
2022-08-21 19:23:13 -0500

    feat: updated index script

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,20 @@ import express from "express";
 import { logger } from "./api/middleware/logger";
 import routes from "./api/routes";
 
-const main = async () => {
-  const PORT = 4000;
+const createServer = () => {
+  // Create HTTP server
   const app = express();
-
+  // Apply middleware
   app.use(express.json());
   app.use(logger);
+  // Connect routes
   app.use("/api", routes);
+  return { app };
+};
+
+const main = async () => {
+  const PORT = 4000;
+  const { app } = createServer();
 
   app.listen(PORT, () => {
     console.log(`✨ Express app listening on http://localhost:${PORT}`);
